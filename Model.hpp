7acd31b2cd386b6ca69e2d9d183a5f5dcfc0739b
Ruan Montelo <ruanmontelo@gmail.com>
2023-09-26 18:11:27 -0300

    Commiting all files in the project

---
+++ Model.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "mesh.hpp"
+
+#include <vector>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+class Model
+{
+public:
+	Model(const char* path, bool gamma = false) : gammaCorrection(gamma)
+	{
+		loadModel(path);
+	}
+
+	void Draw(Shader& shader);
+
+private:
+	std::vector<Mesh> meshes;
+	std::string directory;
+	std::vector<Texture> textures_loaded;
+	bool gammaCorrection;
+
+	void loadModel(string const& path);
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
+	unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma);
+};
